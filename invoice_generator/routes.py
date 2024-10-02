60f5b49f98e0b40f542c0c8d7d502f28e28a10a0
Valeryio <tommagoma23@gmail.com>
2024-10-02 12:52:06 +0100

    We can send a pdf file to a client from our application

--- invoice_generator/routes.py
+++ invoice_generator/routes.py
@@ -1,8 +1,9 @@
 
-from flask import render_template
+from flask import render_template, send_file
 from weasyprint import HTML
 from invoice_generator import app
 from datetime import datetime
+import io
 
 
 @app.route("/")
@@ -11,13 +12,19 @@ def index():
     d = datetime.now()
     new_date = d.strftime("%B") + " " + d.strftime("%d") + ", " + d.strftime("%Y")
 
-    return render_template("invoice.html", date=new_date)
+    file_path = "./invoice_generator/templates/invoice.html"
+    render = render_template("invoice.html", date=new_date)
+
+    my_html = HTML(string=render)
+    my_html.write_pdf("./pdf/invoice.pdf")
+    rendered_pdf = my_html.write_pdf()
+
+    return send_file(io.BytesIO(rendered_pdf), download_name="invoice.pdf")
 
 
 
 def generate_pdf():
 
     file_path = "./invoice_generator/templates/invoice.html"
-
     my_html = HTML(file_path)
     my_html.write_pdf("./invoice.pdf")
